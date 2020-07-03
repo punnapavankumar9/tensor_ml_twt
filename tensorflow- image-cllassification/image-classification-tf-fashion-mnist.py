7d45a4e1921f9baf29c0c5f37636e06ffedd55a4
punnapavankumar9 <punnapavankumar9@gmail.com>
2020-07-03 15:06:39 +0530

    image-classification-tf-keras-fashion-mnist

---
+++ tensorflow- image-cllassification/image-classification-tf-fashion-mnist.py
@@ -0,0 +1,41 @@
+from tensorflow import keras
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128),
+    keras.layers.Dense(10)
+])
+
+model.compile(optimizer='adam',loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss , test_acc = model.evaluate(test_images, test_labels)
+print(test_acc)
+
+predict_model = keras.Sequential([model, keras.layers.Softmax()])
+
+predictions = predict_model.predict(test_images)
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.grid(False)
+    plt.subplot(5,5,i+1)
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[test_labels[i]])
+    plt.title(class_names[np.argmax(predictions[i])])
+    plt.tight_layout(pad=3)
+plt.show()
